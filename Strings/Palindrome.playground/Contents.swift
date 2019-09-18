9938302848c91718a448bc803cb76817052f385f
victor <victor@kaputfacturas.com>
2019-09-19 03:32:19 +0900

    palindrome

---
+++ Strings/Palindrome.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+import XCTest
+
+class Palindrome: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func test(){
+       
+        XCTAssertEqual(isPalindrome(txt: "anita"), false)
+
+        XCTAssertEqual(isPalindrome(txt: "anita lava la tina"), true)
+       
+        XCTAssertEqual(isPalindrome(txt: "reloj"), false)
+    }
+    
+    func isPalindrome(txt:String)->Bool{
+      
+        let clearTxt = txt.replacingOccurrences(of: " ", with: "")
+        let txtLenght = clearTxt.count
+        for i in 0..<txtLenght{
+            if(Array(clearTxt)[i] != Array(clearTxt)[txtLenght-i-1]){
+                return false
+            }
+        }
+        return true
+    }
+}
+
+Palindrome.defaultTestSuite.run()
